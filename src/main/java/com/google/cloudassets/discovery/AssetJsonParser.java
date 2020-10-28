0831569267e6531127c37036028a193c98e1db6c
Yarden van den Brink <yardenvan@google.com>
2020-10-28 11:25:42 +0000

    Added support to Kubernetes Cluster asset

--- src/main/java/com/google/cloudassets/discovery/AssetJsonParser.java
+++ src/main/java/com/google/cloudassets/discovery/AssetJsonParser.java
@@ -8,13 +8,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * The AssetJsonParser class maps a json node into a list of Maps in which each map represents
  * a different asset object data.
  */
 public class AssetJsonParser {
     private List<Map<String,Object>> assetsList;
+    private Map<String, Object> propertiesMap;
 
     private static final ObjectMapper jsonMapper = new ObjectMapper();
 
@@ -24,27 +24,37 @@ public class AssetJsonParser {
      */
     public AssetJsonParser(JsonNode jsonNode, AssetKind assetKind) {
         this.assetsList = new ArrayList<>();
-        Map<String, Object> propertiesMap = jsonMapper.convertValue(jsonNode, Map.class);
+        this.propertiesMap = jsonMapper.convertValue(jsonNode, Map.class);
         switch (assetKind) {
             case APP_APP_ENGINE_ASSET:
                 this.assetsList.add(propertiesMap);
                 break;
             case TOPIC_PUB_SUB_ASSET:
-                this.assetsList = (List<Map<String,Object>>) propertiesMap.get("topics");
+                this.assetsList = getFromPropertiesMap("topics");
                 break;
             case SUBSCRIPTION_PUB_SUB_ASSET:
-                this.assetsList = (List<Map<String,Object>>) propertiesMap.get("subscriptions");
+                this.assetsList = getFromPropertiesMap("subscriptions");
                 break;
             case INSTANCE_SPANNER_ASSET:
-                this.assetsList = (List<Map<String,Object>>) propertiesMap.get("instances");
+                this.assetsList = getFromPropertiesMap("instances");
+                break;
+            case CLUSTER_KUBERNETES_ASSET:
+                this.assetsList = getFromPropertiesMap("clusters");
                 break;
             default:
-                this.assetsList = (List<Map<String,Object>>) propertiesMap.get("items");
+                this.assetsList = getFromPropertiesMap("items");
                 break;
         }
         if (this.assetsList == null) {
             this.assetsList = Collections.emptyList();
         }
+    }
+
+    /*
+    This function returns a list of maps from the propertiesMap based on the given key string.
+     */
+    private List<Map<String,Object>> getFromPropertiesMap(String key) {
+        return (List<Map<String,Object>>) this.propertiesMap.get(key);
     }
 
     /**
